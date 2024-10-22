79123135769237ee70b765a5ecd1a06e60ba41f7
Damien Vergobbi <damienvergobbi@Mini-de-Damien-001.lan>
2024-10-22 19:22:08 +0200

    Avoid server cache

--- Block/React.php
+++ Block/React.php
@@ -5,11 +5,6 @@ use Magento\Framework\View\Element\Template;
 
 class React extends Template
 {
-    public function __construct()
-    {
-        $this->_isScopePrivate = true;
-    }
-
     public function getReactAppUrl()
     {
         return "https://d36h2ac42341sx.cloudfront.net";
