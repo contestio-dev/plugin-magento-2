bf26d2aea88152ac2cab08eb1d86f0297cfb939c
Paulo <pauloaimp@gmail.com>
2025-01-13 17:38:52 +0100

    add Page page

---
+++ Controller/Page/Index.php
@@ -0,0 +1,27 @@
+<?php
+namespace Contestio\Connect\Controller\Profile;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Contestio\Connect\Helper\Data as ApiHelper;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        ApiHelper $apiHelper
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context, $apiHelper);
+    }
+
+    public function execute()
+    {        
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+}
